e42bd46c48bf42b41aa6999f92f7bc34f36adaba
aiswarya <aiswarya180@gmail.com>
2022-03-30 18:54:06 +0530

    s3 buket

--- eventmanagement/settings.py
+++ eventmanagement/settings.py
@@ -227,9 +227,9 @@ REST_FRAMEWORK = {
 # }
 # AWS_LOCATION = 'static'
 #
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_QUERYSTRING_AUTH = False
